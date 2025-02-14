e697b75fb2f388aa9649cf81a7a207bde3a9b1e1
marcin.milczek <marcin.milczek@onwelo.com>
2025-02-14 15:17:02 +0100

    add relationships to entity User, add method isUserOnLine

--- whatsappclone/src/main/java/com/marcin/whatsappclone/user/User.java
+++ whatsappclone/src/main/java/com/marcin/whatsappclone/user/User.java
@@ -1,15 +1,19 @@
 package com.marcin.whatsappclone.user;
 
+import com.marcin.whatsappclone.chat.Chat;
 import com.marcin.whatsappclone.common.BaseAuditingEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,10 +23,26 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 public class User extends BaseAuditingEntity {
 
+    private static final int LAST_ACTIVE_INTERVAL = 5;
     @Id
     private String id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
+
     private LocalDateTime lastSeen;
+
+    @OneToMany(mappedBy = "sender")
+    private List<Chat> chatsAsSender;
+
+    @OneToMany(mappedBy = "recipient")
+    private List<Chat> chatsAsRecipient;
+
+    @Transient
+    public boolean isUserOnline() {
+        return lastSeen != null && lastSeen.isBefore(LocalDateTime.now().minusMinutes(LAST_ACTIVE_INTERVAL));
+    }
 }
