5bf423ec0280dfac64a7165a20c1bdfb80e22db8
marcin.milczek <marcin.milczek@onwelo.com>
2025-02-14 13:31:44 +0100

    Create WhatsApp-clone application, configure security and first entities

---
+++ whatsappclone/src/test/java/com/marcin/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcin.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
