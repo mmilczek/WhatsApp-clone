04a53cdd64d013b75aebe391b210e8a9f59bce8f
marcin.milczek <marcin.milczek@onwelo.com>
2025-02-14 14:36:48 +0100

    create enums MessageState and MessagType

---
+++ whatsappclone/src/main/java/com/marcin/whatsappclone/message/MessageState.java
@@ -0,0 +1,6 @@
+package com.marcin.whatsappclone.message;
+
+public enum MessageState {
+    SENT,
+    SEEN
+}
---
+++ whatsappclone/src/main/java/com/marcin/whatsappclone/message/MessageType.java
@@ -0,0 +1,8 @@
+package com.marcin.whatsappclone.message;
+
+public enum MessageType {
+    TEXT,
+    IMAGE,
+    AUDIO,
+    VIDEO
+}
