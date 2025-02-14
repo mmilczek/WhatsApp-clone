6368615213a216042520110db54c241109b14d50
marcin.milczek <marcin.milczek@onwelo.com>
2025-02-14 15:39:22 +0100

    add fields to Chat entity

--- whatsappclone/src/main/java/com/marcin/whatsappclone/chat/Chat.java
+++ whatsappclone/src/main/java/com/marcin/whatsappclone/chat/Chat.java
@@ -2,6 +2,8 @@ package com.marcin.whatsappclone.chat;
 
 import com.marcin.whatsappclone.common.BaseAuditingEntity;
 import com.marcin.whatsappclone.message.Message;
+import com.marcin.whatsappclone.message.MessageState;
+import com.marcin.whatsappclone.message.MessageType;
 import com.marcin.whatsappclone.user.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,11 +14,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static jakarta.persistence.GenerationType.*;
@@ -44,4 +48,40 @@ public class Chat extends BaseAuditingEntity {
     @OneToMany(mappedBy = "chat", fetch = FetchType.EAGER)
     @OrderBy("createdDate DESC")
     private List<Message> messages;
+
+    @Transient
+    public String getChatName(String senderId) {
+        if (recipient.getId().equals(senderId)) {
+            return sender.getFirstName() + " " + sender.getLastName();
+        }
+        return recipient.getFirstName() + " " + recipient.getLastName();
+    }
+
+    @Transient
+    public long getUnreadMessages(String senderId) {
+        return this.messages
+                .stream()
+                .filter(m -> m.getReceiverId().equals(senderId))
+                .filter(m -> MessageState.SENT == m.getState())
+                .count();
+    }
+
+    @Transient
+    public String getLastMessage() {
+        if (messages != null && !messages.isEmpty()) {
+            if (messages.get(0).getType() != MessageType.TEXT) {
+                return "Attachment";
+            }
+            return messages.get(0).getContent();
+        }
+        return null;
+    }
+
+    @Transient
+    public LocalDateTime getLastMessageTime() {
+        if (messages != null && !messages.isEmpty()) {
+            return messages.get(0).getCreatedDate();
+        }
+        return null;
+    }
 }
